b1a0fdeff3915f6bfba522daf2d4abfe52a1e7c0
Дарья Никитина <lazarus37th@yandex.ru>
2023-06-13 02:30:53 +0300

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,27 @@
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
 
+
+function checkPalindrom (string) {
+  const changedString = string.toLowerCase().replaceAll(' ', '');
+  const reversedString = [changedString].reverse().join('');
+
+  return changedString === reversedString;
+}
+
+function getNumber (string) {
+  string = String(string);
+  let number = '';
+  for (let i = 0; i < string.length; i++) {
+    const symbol = parseInt(string[i], 10);
+    if (Number.isNaN(symbol) === false) {
+      number += symbol;
+    }
+  }
+  return Number(number || string);
+}
+
+checkStringLength('проверямая строка', 10);
+checkPalindrom('Лёша на полке клопа нашёл ');
+getNumber('2023 год');

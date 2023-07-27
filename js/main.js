d11899330f33281c1775a2dac809eb34fcf89698
Дарья Никитина <lazarus37th@yandex.ru>
2023-07-27 18:25:00 +0300

    form.js, main.js, index.html fixed. scale.js and effects.js added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import './form.js';
 import { randomPhotos } from './script.js';
 import { renderGallery } from './gallery.js';
+// import './effects.js';
+// import './scale.js';
 
 renderGallery(randomPhotos());

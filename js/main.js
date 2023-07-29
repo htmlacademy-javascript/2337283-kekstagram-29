3bbf39ba32499dace7e13299cb1dcb87b4f35200
Дарья Никитина <lazarus37th@yandex.ru>
2023-07-29 15:47:04 +0300

    main.js fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import './form.js';
 import { randomPhotos } from './script.js';
 import { renderGallery } from './gallery.js';
-// import './effects.js';
-// import './scale.js';
 
 renderGallery(randomPhotos());
